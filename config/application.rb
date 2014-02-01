d1cbeeca03c4799950171c0b43fde81fb156ef5e
Vishal Patel <me@vish.eu>
2014-02-01 19:03:08 +0000

    Set default locale using initializers

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,7 @@ module ActiveInvoices
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :'en-GB'
+    # Locale set in /config/initializers/locale.rb
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
